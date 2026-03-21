58fedb20c616e07512262a5da30ee1db6c3de426
fabledsaint <35288585+fabledsaint@users.noreply.github.com>
2026-03-22 01:44:12 +1100

    chore: run shadcn-svelte update on all components

--- src/lib/components/ui/alert-dialog/alert-dialog-media.svelte
+++ src/lib/components/ui/alert-dialog/alert-dialog-media.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { cn, type WithElementRef } from "$lib/utils.js";
 	import type { HTMLAttributes } from "svelte/elements";
+	import { cn, type WithElementRef } from "$lib/utils.js";
 
 	let {
 		ref = $bindable(null),
@@ -12,11 +12,8 @@
 
 <div
 	bind:this={ref}
-	data-slot="empty-description"
-	class={cn(
-		"text-sm/relaxed text-muted-foreground [&>a:hover]:text-primary text-sm/relaxed [&>a]:underline [&>a]:underline-offset-4",
-		className
-	)}
+	data-slot="alert-dialog-media"
+	class={cn("bg-muted mb-2 inline-flex size-10 items-center justify-center rounded-md sm:group-data-[size=default]/alert-dialog-content:row-span-2 *:[svg:not([class*='size-'])]:size-6", className)}
 	{...restProps}
 >
 	{@render children?.()}
