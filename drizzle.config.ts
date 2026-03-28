c8b0b7e5a101766109bacd723dccfa324e41e57b
isobelmcrae <isobel@isobel.zip>
2026-03-28 23:16:25 +1100

    feat(db): setup neon migration scripts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.NEON_DATABASE_URL) throw new Error('NEON_DATABASE_URL is not set');
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 	out: './src/lib/server/db/migrations',
-	dialect: 'sqlite',
-	dbCredentials: { url: process.env.DATABASE_URL },
+	dialect: 'postgresql',
+	dbCredentials: { url: process.env.NEON_DATABASE_URL },
 	verbose: true,
 	strict: true
 });
